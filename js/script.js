51a0dcd79f0d9ff1c89a6e34f9b4b01b23166f62
Gogi <1@1.com>
2023-09-20 17:39:40 +0300

    First commit

---
+++ js/script.js
@@ -0,0 +1,22 @@
+"use strict";
+
+//const result = confirm("nen?");
+
+// const answers=[];
+// answers[0]= prompt("ask1?","");
+// answers[1]= prompt("ask2?","");
+// answers[2]= prompt("ask3?","");
+
+// document.write(answers);
+
+
+let incr = 10,
+	decr = 10;
+
+// incr;
+// decr;
+
+console.log(++incr + " // " + --decr);
+
+console.log(5%2);
+console.log(2-4==8);
